3521da26e3c20bf11735018c35c8b66ee150532e
amal wafiah <amalwafiah@gmail.com>
2026-04-18 15:50:18 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// Change text function
+function changeText() {
+    document.getElementById("greeting").innerText = "Welcome to my website 💗";
+}
+
+// Counter function
+let count = 0;
+
+function increaseCount() {
+    count++;
+    document.getElementById("count").innerText = count;
+}
